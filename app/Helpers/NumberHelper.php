5fd20a0b809dbe9b281407597bd5e7698c9076e8
garycdev <admin@garycdev.com>
2025-02-01 15:40:14 -0400

    literal

--- app/Helpers/NumberHelper.php
+++ app/Helpers/NumberHelper.php
@@ -1,64 +1,54 @@
 <?php
 
-
-if (!function_exists('numero_literal')) {
+if (! function_exists('numero_literal')) {
     function numero_literal($numero)
     {
-        $unidades = ['', 'uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve'];
-        $decenas = ['', 'diez', 'veinte', 'treinta', 'cuarenta', 'cincuenta', 'sesenta', 'setenta', 'ochenta', 'noventa'];
-        $centenas = ['', 'ciento', 'doscientos', 'trescientos', 'cuatrocientos', 'quinientos', 'seiscientos', 'setecientos', 'ochocientos', 'novecientos'];
-
         if ($numero == 0) {
             return 'cero';
         }
 
-        if ($numero == 100) {
-            return 'cien';
-        }
+        $unidades   = ['', 'uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve'];
+        $decenas    = ['', 'diez', 'veinte', 'treinta', 'cuarenta', 'cincuenta', 'sesenta', 'setenta', 'ochenta', 'noventa'];
+        $centenas   = ['', 'ciento', 'doscientos', 'trescientos', 'cuatrocientos', 'quinientos', 'seiscientos', 'setecientos', 'ochocientos', 'novecientos'];
+        $especiales = [
+            10 => 'diez', 11   => 'once', 12      => 'doce', 13       => 'trece', 14     => 'catorce',
+            15 => 'quince', 16 => 'dieciséis', 17 => 'diecisiete', 18 => 'dieciocho', 19 => 'diecinueve',
+        ];
 
         $literal = '';
 
-        // Manejar centenas
-        if ($numero >= 100) {
+        if ($numero >= 1000000) {
+            $millones = intval($numero / 1000000);
+            $literal .= ($millones == 1) ? 'un millón ' : numero_literal($millones) . ' millones ';
+            $numero %= 1000000;
+        }
+
+        if ($numero >= 1000) {
+            $miles = intval($numero / 1000);
+            $literal .= ($miles == 1) ? 'mil ' : numero_literal($miles) . ' mil ';
+            $numero %= 1000;
+        }
+
+        if ($numero == 100) {
+            $literal .= 'cien';
+        } elseif ($numero >= 100) {
             $centena = intval($numero / 100);
             $literal .= $centenas[$centena] . ' ';
             $numero %= 100;
         }
 
-        // Manejar decenas
-        if ($numero >= 10 && $numero <= 29) {
-            // Excepciones para 11-19 y "veinti..."
-            $especiales = [
-                10 => 'diez',
-                11 => 'once',
-                12 => 'doce',
-                13 => 'trece',
-                14 => 'catorce',
-                15 => 'quince',
-                16 => 'dieciséis',
-                17 => 'diecisiete',
-                18 => 'dieciocho',
-                19 => 'diecinueve'
-            ];
-
-            if (array_key_exists($numero, $especiales)) {
-                $literal .= $especiales[$numero];
-                $numero = 0;
-            } else {
-                $literal .= 'veinti';
-                $numero -= 20;
-            }
-        } elseif ($numero >= 30) {
+        if ($numero >= 10 && $numero <= 19) {
+            $literal .= $especiales[$numero];
+            $numero = 0;
+        } elseif ($numero >= 20) {
             $decena = intval($numero / 10);
             $literal .= $decenas[$decena];
             $numero %= 10;
-
             if ($numero > 0) {
                 $literal .= ' y ';
             }
         }
 
-        // Manejar unidades
         if ($numero > 0) {
             $literal .= $unidades[$numero];
         }
