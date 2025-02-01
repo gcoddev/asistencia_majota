7302731d79940eb5fa40144e9ebd43db1703fbde
garycdev <admin@garycdev.com>
2025-02-01 15:15:41 -0400

    auth

--- app/Http/Controllers/DetalleController.php
+++ app/Http/Controllers/DetalleController.php
@@ -6,6 +6,7 @@ use App\Models\EmpleadoDetalle;
 use App\Models\Usuario;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DetalleController extends Controller
 {
--- app/Http/Controllers/SueldosController.php
+++ app/Http/Controllers/SueldosController.php
@@ -8,6 +8,7 @@ use App\Models\ReciboItem;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SueldosController extends Controller
 {
